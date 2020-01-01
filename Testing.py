f809c8602d57043e975abb6ca207215c390db8dc
Vathanan <Vathanan@outlook.com>
2020-01-01 16:10:12 +0100

    Project Init 1

--- Testing.py
+++ Testing.py
@@ -1,1 +1,10 @@
-HaHa
+f = open('Data.dat', 'w')
+import numpy as np
+
+n = int(input("Enter number of elements:"))
+d = int(input("Enter number of Dimension :"))
+
+random_matrix_array = np.random.rand(n, d)
+print(random_matrix_array)
+
+f.close()
